0cd07593aba65b5486e26f3e22bd523f0162d52e
Novorock <pasta.chemarmazovich@mail.ru>
2023-04-16 22:58:55 +0300

    org initial commit

---
+++ org/force-app/main/default/lwc/generateInvoiceComponent/generateInvoiceComponent.js
@@ -0,0 +1,42 @@
+import {  LightningElement, api, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+import generateInvoicePdf from '@salesforce/apex/GenerateInvoice.generateInvoicePdf'
+
+export default class GenerateInvoiceComponent extends LightningElement {
+    @api recordId;
+
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+        if (currentPageReference) {
+            this.recordId = currentPageReference.state.recordId;
+        }
+    }
+    
+    @api invoke() {
+        if (this.recordId === null || this.recordId === undefined) {
+            return;
+        }
+        
+        let showSuccessToast = new ShowToastEvent({
+            title: 'Success',
+            message: 'Invoice was generated successfully.',
+            variant: 'success'
+        });
+
+        let showFailToast = new ShowToastEvent({
+            title: 'Fail',
+            message: 'Something went wrong during the invoice generating.',
+            variant: 'error'
+        }); 
+
+        generateInvoicePdf({
+            oppId: this.recordId   
+        }).then(result => {
+            this.dispatchEvent(showSuccessToast);
+        }).catch(error => {
+            this.dispatchEvent(showFailToast);
+            console.log(error);
+        });
+    }
+}
