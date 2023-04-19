5c6381f0c8aca484276f1f70e7015ceb2fe1f58a
Novorock <pasta.chemarmazovich@mail.ru>
2023-04-19 23:30:18 +0300

    available for record page

--- org/force-app/main/default/lwc/opportunityReportPerAccountComponent/opportunityReportPerAccountComponent.js
+++ org/force-app/main/default/lwc/opportunityReportPerAccountComponent/opportunityReportPerAccountComponent.js
@@ -1,70 +1,91 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api} from 'lwc';
 import getAccountOpportunityPage from '@salesforce/apex/OpportunityReportPerAccountController.getAccountOpportunityPage';
 import getProductsDataByOppId from '@salesforce/apex/OpportunityReportPerAccountController.getProductsDataByOppId';
 import getAccountOpportunityPageFiltered from '@salesforce/apex/OpportunityReportPerAccountController.getAccountOpportunityPageFiltered';
+import getAccountOpportunityById from '@salesforce/apex/OpportunityReportPerAccountController.getAccountOpportunityById';
 
 export default class OpportunityReportPerAccountComponent extends LightningElement {
+    // Record Page Stuff
+    @api recordId;
+    @api isRecordPage = false;
+    @api currentRecord = {};
+
+    // Custom Lightning Tab Stuff 
     @api accountsList
     pagesAmount;
     @api currentPage = 1;
     isPaginationShown = false;
-
     isModalOpen = false;
     @api modalContext = {
         title: '',
         products: [],
         empty: true
     };
-
     @api searchContext = {
         accountName: '',
         priceFrom: 0,
         priceTo: 0
     };
-
     searchTimeout = 0;
 
-    retrieveAndUpdate() {
-        if (!this.searchContext.accountName && !this.searchContext.priceFrom && !this.searchContext.priceTo) {
+    connectedCallback() {
+        var component = this;
+        // Initial data retrieve
+
+        if (!component.recordId) {
             getAccountOpportunityPage({
-                pageN: this.currentPage,
+                pageN: 1,
             }).then(result => {
                 let data = JSON.parse(result);
-                this.accountsList = data.accounts;
-                this.pagesAmount = data.pagesAmount;
-                this.isPaginationShown = (this.pagesAmount) > 1;
+                component.accountsList = data.accounts;
+                component.pagesAmount = data.pagesAmount;
+                component.isPaginationShown = (this.pagesAmount) > 1;
             }).catch(error => {
                 console.log(error);
             });
         } else {
-            getAccountOpportunityPageFiltered({
-                pageN: this.currentPage,
-                searchTokens: this.searchContext.accountName,
-                min: this.searchContext.priceFrom,
-                max: this.searchContext.priceTo
+            component.isRecordPage = true;
+
+            getAccountOpportunityById({
+                id: component.recordId
             }).then(result => {
-                let data = JSON.parse(result);
-                this.accountsList = data.accounts;
-                this.pagesAmount = data.pagesAmount;
-                this.isPaginationShown = (this.pagesAmount) > 1;
+                component.currentRecord = JSON.parse(result).accounts[0];
             }).catch(error => {
                 console.log(error);
             });
         }
     }
 
-    connectedCallback() {
-        // Initial data retrieve
-        getAccountOpportunityPage({
-            pageN: 1,
-        }).then(result => {
-            let data = JSON.parse(result);
-            this.accountsList = data.accounts;
-            this.pagesAmount = data.pagesAmount;
-            this.isPaginationShown = (this.pagesAmount) > 1;
-        }).catch(error => {
-            console.log(error);
-        });
+    retrieveAndUpdate() {
+        var component = this;
+
+        if (!component.searchContext.accountName && !component.searchContext.priceFrom && !component.searchContext.priceTo) {
+            
+            getAccountOpportunityPage({
+                pageN: component.currentPage,
+            }).then(result => {
+                let data = JSON.parse(result);
+                component.accountsList = data.accounts;
+                component.pagesAmount = data.pagesAmount;
+                component.isPaginationShown = (component.pagesAmount) > 1;
+            }).catch(error => {
+                console.log(error);
+            });
+        } else {
+            getAccountOpportunityPageFiltered({
+                pageN: component.currentPage,
+                searchTokens: component.searchContext.accountName,
+                min: component.searchContext.priceFrom,
+                max: component.searchContext.priceTo
+            }).then(result => {
+                let data = JSON.parse(result);
+                component.accountsList = data.accounts;
+                component.pagesAmount = data.pagesAmount;
+                component.isPaginationShown = (component.pagesAmount) > 1;
+            }).catch(error => {
+                console.log(error);
+            });
+        }
     }
 
     handleNextPage(event) {
@@ -82,48 +103,56 @@ export default class OpportunityReportPerAccountComponent extends LightningEleme
     }
 
     handleAccountNameChange(event) {
-        clearTimeout(this.searchTimeout);
-        this.searchContext.accountName = event.target.value.trim();
+        var component = this;
 
-        this.searchTimeout = setTimeout(() => {
-            if (event.target.value !== this.searchContext.accountName) {
-                this.currentPage = 1;
-                this.retrieveAndUpdate();
+        clearTimeout(component.searchTimeout);
+        component.searchContext.accountName = event.target.value.trim();
+
+        component.searchTimeout = setTimeout(() => {
+            if (event.target.value !== component.searchContext.accountName) {
+                component.currentPage = 1;
+                component.retrieveAndUpdate();
             }
         }, 1500);
     }
 
     handleTotalPriceFromChange(event) {
-        clearTimeout(this.searchTimeout);
-        this.searchContext.priceFrom = parseInt(event.target.value.trim());
+        var component = this;
 
-        this.searchTimeout = setTimeout(() => {
-            this.currentPage = 1;
-            this.retrieveAndUpdate();
+        clearTimeout(component.searchTimeout);
+        component.searchContext.priceFrom = parseInt(event.target.value.trim());
+
+        component.searchTimeout = setTimeout(() => {
+            component.currentPage = 1;
+            component.retrieveAndUpdate();
         }, 1500);
     }
 
     handleTotalPriceToChange(event) {
-        clearTimeout(this.searchTimeout);
-        this.searchContext.priceTo = parseInt(event.target.value.trim());
+        var component = this;
 
-        this.searchTimeout = setTimeout(() => {
-            this.currentPage = 1;
-            this.retrieveAndUpdate();
+        clearTimeout(component.searchTimeout);
+        component.searchContext.priceTo = parseInt(event.target.value.trim());
+
+        component.searchTimeout = setTimeout(() => {
+            component.currentPage = 1;
+            component.retrieveAndUpdate();
         }, 1500);
     }
 
     openModal(event) {
+        var component = this;
+
         let title = event.target.dataset.title;
 
         getProductsDataByOppId({
             oppId: event.target.dataset.id
         }).then(data => {
             let products = JSON.parse(data);
-            this.modalContext.title = title;
-            this.modalContext.empty = (products.length < 1);
-            this.modalContext.products = products;
-            this.isModalOpen = true;
+            component.modalContext.title = title;
+            component.modalContext.empty = (products.length < 1);
+            component.modalContext.products = products;
+            component.isModalOpen = true;
         }).catch(error => {
             console.log(error);
         });
